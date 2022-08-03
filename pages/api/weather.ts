b02c7dbd8b76ef3bb79c32024c25fbb096affba0
shah62 <52537686+shah62@users.noreply.github.com>
2022-08-02 21:39:59 -0400

    chore: add comments to weather api endpoint file

--- pages/api/weather.ts
+++ pages/api/weather.ts
@@ -25,11 +25,17 @@ export default async function handler(
 	const startTimeDate = new Date(startTime as string).toDateString();
 	const endTimeDate = new Date(endTime as string).toDateString();
 
+	/**
+	 * The cachekey is dependent only the start time's and end time's date part as
+	 * those are sent as an ISO string which makes it hard to cache for a given date
+	 * since the time value in date can change per request.
+	 */
 	const cacheKey = `${lat}-${long}-${startTimeDate}-${endTimeDate}`;
 
 	const cacheValue = await redis.get(cacheKey);
 
 	if (cacheValue) {
+		// Logging for debugging issues
 		console.log(`Cache hit`, { lat, long, startTime, endTime });
 		return res.status(200).json(cacheValue);
 	}
@@ -39,6 +45,10 @@ export default async function handler(
 	const data = await fetch(URL, { method: 'GET' });
 	const result = await data.json();
 
+	/**
+	 * Caching the API response in redis as the API has a rate limit of 50queries/hour
+	 * which causes issues when testing out in quick sucession.
+	 */
 	await redis.set(cacheKey, JSON.stringify(result), {
 		ex: ONE_HOUR_IN_SECONDS
 	});
