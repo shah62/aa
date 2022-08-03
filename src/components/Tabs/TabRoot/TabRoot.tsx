99e0b6d2023f5ce6a8689edb08d2427ea2f7fcce
shah62 <52537686+shah62@users.noreply.github.com>
2022-08-02 21:10:42 -0400

    chore: create TabRoot component
    created TabRoot component to provide stying and a straight-forward api to use "@radix-ui/tabs"

---
+++ src/components/Tabs/TabRoot/TabRoot.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import * as Tabs from '@radix-ui/react-tabs';
+import TabRootStyles from './TabRoot.module.scss';
+import clsx from 'clsx';
+
+interface TabTrigger {
+	label: string;
+	value: string;
+	tabTriggerProps?: Omit<Tabs.TabsTriggerProps, 'value' | 'children'>;
+}
+
+export interface TabRootProps {
+	tabsRootProps?: Tabs.TabsProps;
+	tabsListProps?: Tabs.TabsListProps;
+	tabTriggers: TabTrigger[];
+	children: React.ReactNode | ((tabTriggers: TabTrigger) => React.ReactNode);
+}
+
+class TabRoot extends React.Component<TabRootProps> {
+	constructor(props: TabRootProps) {
+		super(props);
+	}
+
+	render(): React.ReactNode {
+		return (
+			<Tabs.Root
+				activationMode='manual'
+				{...this.props.tabsRootProps}
+				className={clsx(
+					TabRootStyles.root,
+					this.props.tabsRootProps?.className
+				)}
+			>
+				<Tabs.List
+					{...this.props.tabsListProps}
+					className={clsx(
+						TabRootStyles.list,
+						this.props.tabsListProps?.className
+					)}
+				>
+					{this.props.tabTriggers.map(({ value, label, tabTriggerProps }) => {
+						return (
+							<Tabs.Trigger
+								key={`tab-${label}-${value}`}
+								{...tabTriggerProps}
+								className={clsx(
+									TabRootStyles.trigger,
+									tabTriggerProps?.className
+								)}
+								value={value}
+							>
+								{label}
+							</Tabs.Trigger>
+						);
+					})}
+				</Tabs.List>
+				{typeof this.props.children === 'function'
+					? this.props.tabTriggers.map((tabTrigger) =>
+							(this.props.children as any)(tabTrigger)
+					  )
+					: this.props.children}
+			</Tabs.Root>
+		);
+	}
+}
+
+export default TabRoot;
