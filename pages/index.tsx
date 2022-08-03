a70333e741797e62fc8ec2fb88f73c634ccde53d
shah62 <52537686+shah62@users.noreply.github.com>
2022-08-02 21:56:21 -0400

    refactor: separate out WeatherDisplay Tab's UI into a separate component from index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,11 @@
 import type { NextPage } from 'next';
+import WeatherDisplayTabs from '../src/components/WeatherDisplayTabs/WeatherDisplayTabs';
 import styles from '../styles/Home.module.scss';
-import TabRoot from '../src/components/Tabs/TabRoot/TabRoot';
-import TabContent from '../src/components/Tabs/TabContent/TabContent';
-import WeatherDisplay from '../src/components/WeatherDisplay/WeatherDisplay';
-import { cityTabMap, cityTabs } from '../src/data/cityTabs';
 
 const Home: NextPage = () => {
-	const defaultValue = cityTabMap.get('ottawa')?.value;
-
 	return (
 		<main className={styles.main}>
-			<TabRoot
-				tabTriggers={cityTabs}
-				tabsRootProps={{ defaultValue: defaultValue }}
-			>
-				{cityTabs.map(({ value }) => {
-					const cityCoords = cityTabMap.get(value)?.coords;
-					return (
-						<TabContent key={`city-tab-content-${value}`} value={value}>
-							<WeatherDisplay coords={cityCoords!} />
-						</TabContent>
-					);
-				})}
-			</TabRoot>
+			<WeatherDisplayTabs />
 		</main>
 	);
 };
---
+++ src/components/WeatherDisplayTabs/WeatherDisplayTabs.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { cityTabs, cityTabMap } from '../../data/cityTabs';
+import TabContent from '../Tabs/TabContent/TabContent';
+import TabRoot from '../Tabs/TabRoot/TabRoot';
+import WeatherDisplay from '../WeatherDisplay/WeatherDisplay';
+
+class WeatherDisplayTabs extends React.Component {
+	constructor(props: unknown) {
+		super(props!);
+	}
+
+	render(): React.ReactNode {
+		const defaultValue = cityTabMap.get('ottawa')?.value;
+		return (
+			<TabRoot
+				tabTriggers={cityTabs}
+				tabsRootProps={{ defaultValue: defaultValue }}
+			>
+				{cityTabs.map(({ value }) => {
+					const cityCoords = cityTabMap.get(value)?.coords;
+					return (
+						<TabContent key={`city-tab-content-${value}`} value={value}>
+							<WeatherDisplay coords={cityCoords!} />
+						</TabContent>
+					);
+				})}
+			</TabRoot>
+		);
+	}
+}
+
+export default WeatherDisplayTabs;
